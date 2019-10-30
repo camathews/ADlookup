6f1396e1644700ba37c6252bf968d25d5d843bf9
camathews <chanmathews@gmail.com>
2019-10-30 09:52:56 -0500

    Updating the Master branch.
    Added the ADlookup script and updated the readme.

---
+++ ADlookup.ps1
@@ -0,0 +1,31 @@
+﻿function lookup {
+    param([string]$username)
+    Write-Host $username':'
+        try {Get-ADUser $username -Properties * | Select-Object -Property EmailAddress, EmployeeType, extensionAttribute1, Department, PasswordLastSet | Format-List}
+        catch {Write-Warning -Message "User not found" }
+    }
+
+$useTxtFile = Read-Host -Prompt 'Use a text file? Y/N'
+
+If ($useTxtFile -ieq 'Y') { 
+    Write-Host "Enter the file path - " -NoNewline
+    $file = $Host.UI.ReadLine() 
+    foreach($line in Get-Content $file) { lookup $line } 
+    }
+     
+Else{
+    $list = New-Object Collections.Generic.List[string]
+    Write-Host 'Enter user name(s) or enter no data to start look up`n'
+    $count = 1
+    Do{
+        $userIn = Read-Host -Prompt "Username $count"
+        if($userIn -ne "") {$list.Add($userIn)}
+        else{break}
+        $count++
+        }While($true)
+     
+     write-host "`n"
+     For($i=0; $i -lt $list.count; $i++) {lookup $list[$i]} 
+    } 
+
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ADlookup
- A powershell script that looks up specfic attributes in Active Directory. 
+ A Powershell script that looks up specific attributes in Active Directory.
+
+ This code requires Remote Server Administration Tools to be installed prior to use.
+ RSAT download can be found on Microsoft.com or at https://www.microsoft.com/en-us/download/details.aspx?id=45520.
+
+ The ADlookup script queries Active Directory for attributes concerning a provided user. This code can also be used to look up several users as command line arguments or as a list stored in a text file.
